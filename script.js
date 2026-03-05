cab655eb077c2c08289504c0c2c4d36a83de409a
Microwave2000 <nagyzsadam@gmail.com>
2026-03-05 17:42:20 +0100

    new grid button ready

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 256; i++) {
-  const grid = document.createElement("div");
-  grid.className = "grid";
-  container.appendChild(grid);
-  grid.addEventListener("mouseover", () => {
-    grid.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
-  });
-}
-
 function randomRGB() {
   return Math.floor(Math.random() * 256);
+}
+
+const newGrid = document.getElementById("newGrid");
+const containerSize = 640;
+
+createGrid(16);
+
+newGrid.addEventListener("click", () => createGrid());
+
+function createGrid(numOfSides) {
+  while (!numOfSides) {
+    const input = prompt("Enter the number of sides of the new grid (1-100)");
+
+    if (input === null) return;
+
+    const value = Number(input);
+
+    if (value >= 1 && value <= 100) {
+      numOfSides = value;
+    }
+  }
+
+  const cellSize = containerSize / numOfSides;
+  container.innerHTML = "";
+
+  for (let i = 0; i < numOfSides ** 2; i++) {
+    const grid = document.createElement("div");
+    grid.className = "grid";
+    grid.style.width = `${cellSize}px`;
+    grid.style.height = `${cellSize}px`;
+
+    container.appendChild(grid);
+
+    grid.addEventListener("mouseover", () => {
+      grid.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+    });
+  }
 }
